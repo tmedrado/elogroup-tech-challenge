0a4fd91140f59261b183e0f19f8956756eaf20a4
Tomaz Medrado <tmedrado99@gmail.com>
2020-11-19 18:01:43 -0300

    fixed validations

--- src/validateInfo.js
+++ src/validateInfo.js
@@ -1,13 +1,17 @@
 export default function validateInfo(values) {
   let errors = {};
+  const passwordRegex = new RegExp(
+    "^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
+  );
 
   if (!values.username.trim()) {
     errors.username = "Coloque um nome de usuário válido";
   }
   if (!values.password) {
     errors.password = "Esse campo é obrigatório";
-  } else if (values.password.length < 6) {
-    errors.password = "Sua senha deve ter 6 ou mais caracteres.";
+  } else if (!passwordRegex.test(values.password)) {
+    errors.password =
+      "Sua senha deve ter ao menos 8 caracteres, um caracter especial, um caracter numérico e um caracter alfanumérico";
   }
 
   if (!values.password2) {
